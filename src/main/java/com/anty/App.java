7da2da09ef229cff15b7e76887296a36e7918ca2
Mateusz Szostok <szostok.mateusz@gmail.com>
2016-04-13 21:38:35 +0200

    Initial commit, add README.md and project structure

---
+++ src/main/java/com/anty/App.java
@@ -0,0 +1,13 @@
+package com.anty;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
